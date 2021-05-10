8b154d2d4c9b058eb8461ec0e36d2cbbb1655b34
rarayukawa <r1u2u18@gmail.com>
2021-05-10 12:43:56 +0900

    random

---
+++ 23-main.js
@@ -0,0 +1,15 @@
+Math.random();
+Math.floor(Math.random() * 100);
+10 + Math.floor(Math.random() * 10);
+
+const rectangle = document.querySelector('.rectangle');
+
+document.querySelector('.button').addEventListener('click', onClickButton);
+
+function onClickButton() {
+  const randomHue = Math.trunc(Math.random() * 360);
+  const randomColorStart = `hsl(${randomHue}, 100%, 50%)`;
+  const randomColorEnd = `hsl(${randomHue + 40}, 100%, 50%)`;
+  rectangle.style.setProperty('--start', randomColorStart);
+  rectangle.style.setProperty('--end', randomColorEnd);
+}
