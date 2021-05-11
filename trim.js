6636ac84d35846ebecabc4c80cbcaf7a19cfcbc8
rarayukawa <r1u2u18@gmail.com>
2021-05-11 10:10:03 +0900

    28-trim

---
+++ trim.js
@@ -0,0 +1,12 @@
+//両橋にスペースがある場合
+const targetString1 = '　こんにちは　';
+const trimmedString1 = targetString1.trim();
+console.log(trimmedString1);
+
+const targetString2 = ' りんごを食べました ';
+const trimmedString2 = targetString2.trim();
+console.log(trimmedString2);
+
+const targetString3 = ' りんご。　みかん ';
+const trimmedString3 = targetString3.trim();
+console.log(trimmedString3);
