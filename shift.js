f368cfb378afaf0319f5c6059a837f2badacdba7
rarayukawa <r1u2u18@gmail.com>
2021-05-13 12:01:43 +0900

    splice

---
+++ shift.js
@@ -0,0 +1,9 @@
+const array1 = ['りんご', 'みかん', 'バナナ'];
+const shiftedValue = array1.shift();
+console.log(shiftedValue);
+console.log(array1);
+
+const array2 = ['りんご', 'みかん', 'バナナ'];
+const poppedValue = array2.pop();
+console.log(poppedValue);
+console.log(array2);
---
+++ splice.js
@@ -0,0 +1,7 @@
+const array3 = ['りんご', 'みかん'];
+array3.splice(1, 0, 'バナナ');
+console.log(array3);
+
+const array4 = ['りんご', 'みかん'];
+array4.splice(1, 1, 'バナナ', 'いちご');
+console.log(array4);
