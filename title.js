119b030027a775637570facc27a44dc2e278ab62
rarayukawa <r1u2u18@gmail.com>
2021-05-14 23:00:44 +0900

    changetitle

---
+++ title.js
@@ -0,0 +1,7 @@
+document.querySelector('#btnApple').addEventListener('click', () => {
+  document.title = 'アポー';
+});
+
+document.querySelector('#btnOrange').addEventListener('click', () => {
+  document.title = 'オランジェ';
+});
