e83614bc352885590daa01a3b7515e71998ff9f6
rarayukawa <r1u2u18@gmail.com>
2021-05-19 00:02:24 +0900

    style

---
+++ style.js
@@ -0,0 +1,8 @@
+const information = document.querySelector('#information');
+information.style.color = 'white';
+information.style.fontSize = '70px';
+information.style.fontWeight = '600';
+
+const strokeColor = '#c52b84';
+information.style.webkitTextStroke = `2px ${strokeColor}`;
+information.style.textShadow = `7px 7px 0 #bf3384`;
