db6a32e583e7bf1a28c4cd1603cdb4d5e456be40
rarayukawa <r1u2u18@gmail.com>
2021-05-19 13:47:51 +0900

    checked

---
+++ checked.js
@@ -0,0 +1,12 @@
+const cbA = document.querySelector('#cbA');
+const checkedA = cbA.checked;
+
+const cbB = document.querySelector('#cbB');
+const checkedB = cbB.checked;
+
+const cbC = document.querySelector('#cbC');
+const checkedC = cbC.checked;
+
+console.log('checkedAの値', checkedA);
+console.log('checkedBの値', checkedB);
+console.log('checkedCの値', checkedC);
