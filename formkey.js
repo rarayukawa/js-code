ab3330fc900492edde35efda3189905b6849765e
rarayukawa <r1u2u18@gmail.com>
2021-05-19 14:28:09 +0900

    formkey

---
+++ formkey.js
@@ -0,0 +1,6 @@
+const element = document.querySelector('form#radioGroup');
+const drinkValue = element.drink.value;
+const fruitValue = element.fruit.value;
+
+console.log(`drinkの値は ${drinkValue}です`);
+console.log(`fruitValueの値は ${fruitValue}　です`);
